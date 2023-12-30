df3b0ad2509702a472b5852001bcc6ceae0bc6e2
Not Today <48966874+another-salad@users.noreply.github.com>
2023-12-30 00:41:36 +0000

    Useful public methods

--- dashboard/dbApp/TempSensors.cs
+++ dashboard/dbApp/TempSensors.cs
@@ -6,7 +6,7 @@ using MySql.EntityFrameworkCore.Extensions;
 namespace mysqlefcore {
     public class tempCData {
         public int id { get; set; }
-        public DateTime timestamp { get; }
+        public DateTime timestamp { get; set; }
         public double temperatureC { get; set; }
         public string? sensorName { get; set; }
     }
@@ -14,6 +14,10 @@ namespace mysqlefcore {
     public class TempSensorDbContext(string DBConnectionString) : DbContext {
 
         public DbSet<tempCData> tempCData { get; set; }
+
+        private readonly int CurrentYear = DateTime.Now.Year;
+
+        private readonly int CurrentMonth = DateTime.Now.Month;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             optionsBuilder.UseMySQL(DBConnectionString);
@@ -24,9 +28,43 @@ namespace mysqlefcore {
                 temperatureC = tempC,
                 sensorName = sensorName
             };
-            using var db = new TempSensorDbContext(DBConnectionString);
-            db.tempCData.Add(record);
-            db.SaveChanges();
+            tempCData.Add(record);
+            SaveChanges();
+        }
+
+        public async Task<List<tempCData>> GetAllRecords() {
+            var records = await tempCData.ToListAsync();
+            return records;
+        }
+
+        public async Task<List<tempCData>> GetRecordsBySensorName(string sensorName) {
+            var records = await tempCData.Where(x => x.sensorName == sensorName).ToListAsync();
+            return records;
+        }
+
+        private static int ReturnValue(int value, int defaultValue) {
+            if (value == -1) {
+                return defaultValue;
+            }
+            return value;
+        }
+
+        private int GetYear(int year) {
+            return TempSensorDbContext.ReturnValue(year, CurrentYear);
+        }
+
+        private int GetMonth(int month) {
+            return TempSensorDbContext.ReturnValue(month, CurrentMonth);
+        }
+
+        public async Task<List<tempCData>> GetAllRecordsInMonth(int month = -1, int year = -1) {
+            var records = await tempCData.Where(x => x.timestamp.Month == GetMonth(month) && x.timestamp.Year == GetYear(year)).ToListAsync();
+            return records;
+        }
+
+        public async Task<List<tempCData>> GetAllRecordsInYear(int year = -1) {
+            var records = await tempCData.Where(x => x.timestamp.Year == GetYear(year)).ToListAsync();
+            return records;
         }
 
     }
